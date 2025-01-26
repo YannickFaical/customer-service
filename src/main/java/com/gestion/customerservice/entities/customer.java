57726eeef25ba62f26f2683a3e062e5c589dff0a
YannickFaical <ycompaore48@gmail.com>
2025-01-26 13:21:09 +0100

    add customer entity

---
+++ src/main/java/com/gestion/customerservice/entities/customer.java
@@ -0,0 +1,14 @@
+package com.gestion.customerservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+}
