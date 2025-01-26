3067ddcf13be11301b174be82627c1a36a2e1f38
YannickFaical <ycompaore48@gmail.com>
2025-01-26 14:19:16 +0100

    test customer repo with RepositoryRestResource

--- src/main/java/com/gestion/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/gestion/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.gestion.customerservice;
 
+import com.gestion.customerservice.entities.Customer;
+import com.gestion.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,18 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner save(CustomerRepository customerRepository){
+        return args ->{
+            Customer customer=Customer.builder()
+                    .name("yannick")
+                    .email("ycompaore48@gmail.com")
+                    .build();
+            customerRepository.save(customer);
+
+            // Affichage pour vérifier si l'enregistrement est bien effectué
+            customerRepository.findAll().forEach(System.out::println);
+        };
+
+    }
 }
--- src/main/java/com/gestion/customerservice/entities/Customer.java
+++ src/main/java/com/gestion/customerservice/entities/Customer.java
@@ -3,9 +3,12 @@ package com.gestion.customerservice.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-@Entity
-public class customer {
+@Entity @NoArgsConstructor @AllArgsConstructor @Builder
+public class Customer {
     @Id
     @GeneratedValue
     private Long id;
